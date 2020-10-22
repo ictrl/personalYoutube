c374a1f4bd66f5b627bf94bfa98eab91d9b81411
Samrat Saurav Jaiswal <coolsaurav34@gmail.com>
2020-10-22 12:54:30 +0530

    add produciton code

--- server.js
+++ server.js
@@ -72,12 +72,12 @@ app.get("/delete/:fileName", (req, res) => {
   });
 });
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 
